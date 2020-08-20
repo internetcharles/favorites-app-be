21f255fabd95a51281deda4ed7c867631a6c0de4
internetcharles <charlesodellsmith@gmail.com>
2020-08-20 13:35:14 -0700

    first commit

---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const movies = require('./movies.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      animals.map(animal => {
+        return client.query(`
+                    INSERT INTO movies (title, rating, image, overview, owner_id)
+                    VALUES ($1, $2, $3, $4, $5);
+                `,
+        [movies.title, movies.rating, movies.image, movies.overview, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
---
+++ data/movies.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    title: 'cat movie',
+    rating: 10, 
+    image: 'cat.jpeg', 
+    overview: 'cat jumps', 
+    owner_id: 1,
+  },
+];
